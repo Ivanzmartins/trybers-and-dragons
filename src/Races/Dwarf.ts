72a40e685d6ab6b05cc864bf5902f187a3b293d8
Ivanzmartins <ivanzmartins@gmail.com>
2023-01-19 19:19:42 -0300

    Requisitos 4 e 5

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -7,11 +7,7 @@ export default class Dwarf extends Race {
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this.maxLifePoints = 80;
-    Dwarf.addCounter();
-  }
-
-  private static addCounter() {
-    this.raceCounter += 1;
+    Dwarf.raceCounter += 1;
   }
 
   static createdRacesInstances(): number {
